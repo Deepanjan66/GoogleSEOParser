403e9ec909e69cea4b031051514207a3dc60cade
Deepanjan <deepanjan.chakrabarty@student.unsw.edu.au>
2019-09-03 21:56:35 +1000

    Remove empty class and remove redundant test cases

--- SEOParser.UnitTest/MyGoogleApiUnitTest.cs
+++ SEOParser.UnitTest/MyGoogleApiUnitTest.cs
@@ -4,72 +4,29 @@ using System.Collections;
 using SEOParser.Helpers;
 
 
-// Testing have only been done for invalid cases as testing for
-// valid cases will may result in a block from google
 namespace SEOParser.UnitTest
 {
     public class GoogleSEORankingUnitTest
     {
 
         [Fact]
-        public void Test1()
+        public void Test_Empty_URL_Should_Return_Zero_Id()
         {
             var api = new MyGoogleApi();
-            ArrayList results = api.getSEORankings("", "sympli", 10, false);
+            ArrayList results = api.getSEORankings("", "somewebsite", 10, false);
 
             Assert.True((int) results[0] == 0);
             Assert.True(results.Count == 1);
         }
 
         [Fact]
-        public void Test2()
+        public void Test_Empty_Keyword_Should_Return_Zero_Id()
         {
             var api = new MyGoogleApi();
-            ArrayList results = api.getSEORankings("sympli.com", "", 10, false);
+            ArrayList results = api.getSEORankings("somewebsite.com", "", 10, false);
 
             Assert.True((int)results[0] == 0);
             Assert.True(results.Count == 1);
         }
-
-        [Fact]
-        public void Test3()
-        {
-            var api = new MyGoogleApi();
-            ArrayList results = api.getSEORankings("", "", 10, false);
-
-            Assert.True((int)results[0] == 0);
-            Assert.True(results.Count == 1);
-        }
-
-        [Fact]
-        public void Test4()
-        {
-            var api = new MyGoogleApi();
-            ArrayList results = api.getSEORankings("", "", 10, true);
-
-            Assert.True((int)results[0] == 0);
-            Assert.True(results.Count == 1);
-        }
-
-        [Fact]
-        public void Test5()
-        {
-            var api = new MyGoogleApi();
-            ArrayList results = api.getSEORankings("sympli.com", "", 10, true);
-
-            Assert.True((int)results[0] == 0);
-            Assert.True(results.Count == 1);
-        }
-
-        [Fact]
-        public void Test6()
-        {
-            var api = new MyGoogleApi();
-            ArrayList results = api.getSEORankings("sympli.com", "sympli", -1, true);
-
-            Assert.True((int)results[0] == 0);
-            Assert.True(results.Count == 1);
-        }
-
     }
 }
